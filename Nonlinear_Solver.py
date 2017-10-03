d259fcd3753a2e39467eb953bf4d8dbe3c8baf57
chrisbso <chrisbso@stud.ntnu.no>
2017-10-03 17:26:28 +0200

    Fikset Newton. Programmet er nå helt kjørbar med passende XML-fil.

--- Nonlinear_Solver.py
+++ Nonlinear_Solver.py
@@ -7,52 +7,56 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 ################################################################################
 
-ERRORTOL = 1e-14
-ITERATIONCAP = 1000
-EPS = 1e-20
 
-TITLESIZE = 18
-LABELSIZE = 14
-plt.style.use('ggplot')
-
-def Newton(f, df, x0):
+def Newton(f, df, x0,errTol):
+    iterationcap = 1001
+    #eps = 1e-20
     x = x0
     xvalues = [x]
     err = []
-    for i in range (ITERATIONCAP):
+    for i in range (iterationcap):
         df_x = df(x)
-        assert(np.abs(df_x) > EPS)
         x = x - f(x) / df(x)
         current_error = np.abs(xvalues[-1] - x)
         xvalues.append(x)
         err.append(current_error)
-        if (current_error < ERRORTOL):
+        if (current_error < errTol):
             break
-    assert(err[-1] < ERRORTOL)
-    return x, err
     
-def Improved_Newton (f, df, ddf, x0):
+    if (i == 1000):
+        result = 'The method failed, and reached the maximum amount of iterations.\n'
+    else:
+        result = ('The root is ' + repr(x) + ', \nwith i = ' + repr(i) + '# of iterations.\nThe errors e_k = |x_k - x_k+1| are as follows, with k = {1, ... ,i}:\n'  +   repr(err) +'\n')
+    print(result)
+    return [x, err]
+    
+def Improved_Newton (f, df, ddf, x0,errTol):
+    iterationcap = 1001
+    #eps = 1e-20
     x = x0
     xvalues = [x]
     err = []
-    for i in range (ITERATIONCAP):
+    for i in range (iterationcap):
         f_x = f(x)
         df_x = df(x)
-        assert(np.abs(df_x) > EPS)
         ddf_x = ddf(x)
-        denominator = df_x * (1 - f_x * ddf_x / df_x**2)
-        assert(np.abs(denominator) > EPS)
-        x = x - f_x / denominator
-        current_error = np.abs(xvalues[-1] - 1)
+        denominator = (1 - f_x * ddf_x / df_x**2)
+        x = x - (f_x) / ((df_x) * denominator)
+        current_error = np.abs(xvalues[-1] - x)
         xvalues.append(x)
         err.append(current_error)
-        if (current_error < ERRORTOL):
+        if (current_error < errTol):
             break
-    assert(err[-1] < ERRORTOL)
-    return x, err
+    if (i == 1000):
+        result = 'The method failed, and reached the maximum amount of iterations.\n'
+    else:
+        result = ('The root is ' + repr(x) + ', \nwith i = ' + repr(i) + '# of iterations.\nThe errors e_k = |x_k - x_k+1| are as follows, with k = {1, ... ,i}:\n'  + 
+        repr(err) +'\n')
+    print(result)
 
-################################################################################
-#define the original Olver method
+    return [x, err]
+
+#define the original Olver's method
 def Olver(f,df,d2f,x0,errTol):
     x = [x0]        #initialize x
     
@@ -75,22 +79,19 @@ def Olver(f,df,d2f,x0,errTol):
         repr(err) +'\n')
         
     print(result)
-    return [x[-1],err,len(err)]
-################################################################################
+    return [x[-1],err]
     
     
-################################################################################
-#define the Improved Olver method by definition of the original algorithm
+#define the Improved Olver's method by definition of the original algorithm
 def Improved_Olver(f,df,d2f,d3f,d4f,x0,errTol): 
     
     g   = lambda x: f(x)/df(x) + d2f(x)*f(x)**2/(2*df(x)**3)
     dg  = lambda x: 1  +  d3f(x)*f(x)**2/(2*df(x)**3)  -  3*d2f(x)**2*f(x)**2/(2*df(x)**4)
     d2g = lambda x: (d4f(x)*f(x)**2/(2*df(x)**3)  +  d3f(x)*f(x)/df(x)**2  - 9*d3f(x)*f(x)**2*d2f(x)/(2*df(x)**4) - 3*d2f(x)**2*f(x)/df(x)**3  + 6*d2f(x)**3*f(x)**2/df(x)**5)
     return Olver(g,dg,d2g,x0,errTol)
-################################################################################
 
     
-################################################################################
+
 #plot any function, given its function handle, on interval [a,b]. fname is the string containing its closed form.
 def Plot_Function(f,fname,a,b):
     x = np.arange(a,b,abs(b-a)/10000)
@@ -101,10 +102,9 @@ def Plot_Function(f,fname,a,b):
     plt.xlabel('x')
     plt.ylabel('f(x)')
     plt.legend()
-################################################################################
 
 
-################################################################################  
+
 #plot the convergence of the method used, given the error array (e_k)_k with element e_k = |x_k - x_k+1|.  here, (fname) is the closed form of the function, while methodname is the name of the method used, both as a string.
 def Plot_Convergence(err,fname,methodname): 
     y = np.zeros((3,len(err)-1))
@@ -134,10 +134,10 @@ def Plot_Convergence(err,fname,methodname):
     plt.ylabel('e_k+1/e_k^3')
     
     plt.show()
-################################################################################
+
         
         
-################################################################################        
+#extract from XML file.
 def XML_Extraction(filename):    
     XMLFILE = filename          #name of xml-file
     tree = et.parse(XMLFILE)
@@ -155,12 +155,11 @@ def XML_Extraction(filename):
     b =         float(root[9].text)
 
     return [method,fname,f,df,d2f,d3f,d4f,errTol,x0,a,b]
-################################################################################
+
     
 
-################################################################################    
+   
 def main():
-    
     data = XML_Extraction('Nonlinear_Solver.xml')
     
     method =    data[0]
@@ -180,44 +179,37 @@ def main():
     
     switch = method
     if  switch == 'olver':
+        print('Olver\'s Method')
         errOlver =      Olver(f,df,d2f,x0,errTol)
         Plot_Convergence(errOlver[1],fname,'Olver\'s Method')
     elif switch == 'improved_olver':
+        print('Improved Olver\'s Method')
         errImpOlver =   Improved_Olver(f,df,d2f,d3f,d4f,x0,errTol)
         Plot_Convergence(errImpOlver[1],fname,'Improved Olver\'s Method')
     elif switch == 'newton':
+        print('Newton\'s Method')
         errNetwon =     Newton(f,df,x0,errTol)
-        #Plot_Convergence(errNewton[1],fname,'Newton\'s method')
+        Plot_Convergence(errNewton[1],fname,'Newton\'s method')
     elif switch == 'improved_newton':
-        errImpNewton =  Improved_Newton(f,df,x0,errTol)
-        #Plot_Convergence(errImpNewton[1],fname,'Improved Newton\'s method')
+        print('Improved Newton\'s Method')
+        errImpNewton =  Improved_Newton(f,df,d2f,x0,errTol)
+        Plot_Convergence(errImpNewton[1],fname,'Improved Newton\'s method')
     elif switch == 'all':
+        print('Olver\'s Method')
         errOlver =      Olver(f,df,d2f,x0,errTol)
+        print('Improved Olver\'s Method')
         errImpOlver =   Improved_Olver(f,df,d2f,d3f,d4f,x0,errTol)
-        errNetwon =     Newton(f,df,x0,errTol)
+        print('Newton\'s Method')
+        errNewton =     Newton(f,df,x0,errTol)
+        print('Improved Newton\'s Method')
         errImpNewton =  Improved_Newton(f,df,d2f,x0,errTol)
         Plot_Convergence(errOlver[1],fname,'Olver\'s Method')
         Plot_Convergence(errImpOlver[1],fname,'Improved Olver\'s Method')
-        #Plot_Convergence(errNewton[1],fname,'Newton\'s method')
-        #Plot_Convergence(errImpNewton[1],fname,'Improved Newton\'s method')
+        Plot_Convergence(errNewton[1],fname,'Newton\'s method')
+        Plot_Convergence(errImpNewton[1],fname,'Improved Newton\'s method')
     else:
         print('Exiting program emptyhanded - please input a correct method, either\n "olver", "improved_olver", "newton", "improved_newton" or "all"\n')
-################################################################################    
+  
 
 if __name__ == "__main__":
     main()
-        
-
-
-# F = lambda x,y,z: np.array([3*x**2-y**2+z**2-1, y**4+3*z-1.5, x+y+4*z**2-2])
-# 
-# J = lambda x,y,z: np.matrix([[6*x, 2*y, 2*z], [0, 4*y**3, 3], [1, 1, 8*z]])
-# 
-# 
-# print(F(1,1,1))
-# 
-# print('\n', J(1,1,1))
-# 
-# print('\n', np.linalg.det(J(1,1,1)))
-# def Nonlinear_Solver_R3():
-# 	return 0
