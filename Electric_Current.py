d294a20363eb3a7bdb3f1c077b52d22b9fb3a13c
Erik Dengerud <erikde@ntnu.no>
2017-10-04 16:09:19 +0200

    Gjort oppg 2d og ført inn i LaTex.

--- Electric_Current.py
+++ Electric_Current.py
@@ -15,46 +15,48 @@ with multivariate newton's method.
 XMLFILE = 'Electric_Current.xml'#name of xml-file
 tree = et.parse(XMLFILE)
 root = tree.getroot()
-R1 = float(root[0][0].text)
-R2 = float(root[0][1].text)
-R3 = float(root[0][2].text)
-R4 = float(root[0][3].text)
-R5 = float(root[0][4].text)
-R6 = float(root[0][5].text)
+R1 = float(root[0].text)
+R2 = float(root[1].text)
+R3 = float(root[2].text)
+R4 = float(root[3].text)
+R5 = float(root[4].text)
+
 
 #Resistances
-R = np.array([R1, R2, R3, R4, R5, R6])
+Rvec = np.array([R1, R2, R3, R4, R5])
 
 #Currents
-I = np.array([1,1,1])
+Ivec = np.array([1,1,1])
 
 #Power 
 P = 0.4
+I = (P*Rvec[4])**0.5
 
 #Tolerance
 tol = 1e-14
 
 #Defining the system as F=(f1, f2, f3....) 
-F = lambda R, I: np.array([3*x**2+7*z-2, 5*x**2+x+y**4-8*z+2, -x**5+y**3+4*y+5*z-1])
+F = lambda Rvec, Ivec: np.array([Ivec[0] + Ivec[1] - I, Ivec[0]*(Rvec[0]-Rvec[3]) - Ivec[2]*Rvec[3] - I*Rvec[4], 
+	Ivec[0]*Rvec[0] + Ivec[1]*Rvec[1] + Ivec[2]*Rvec[2]])
 
 #Defining the jacobi matrix of the system
-J = lambda R, I: np.matrix([[6*x, 0, 7], [10*x+1, 4*y**3, -8], [-5*x**4, 3*y**2+4, 5]])
+J = lambda Rvec: np.matrix([[1, 1, 0], [Rvec[0]-Rvec[3], 0, -Rvec[3]], [Rvec[0], Rvec[1], Rvec[2]]])
 
-def Multvariate_Newton(I):
-	print('Starting at: ', I)
-	F_value = F(R,I)
+def Multvariate_Newton(Ivec, Rvec):
+	print('Starting at: ', Ivec)
+	F_value = F(Rvec,Ivec)
 	F_norm = np.linalg.norm(F_value, ord=np.inf)
 	itcount = 0
 	while abs(F_norm)>tol and itcount<100:
-		delta = np.linalg.solve(J(R, I), -F_value)
-		I = I +delta
-		F_value = F(R, I)
+		delta = np.linalg.solve(J(Rvec), -F_value)
+		Ivec = Ivec +delta
+		F_value = F(Rvec, Ivec)
 		F_norm = np.linalg.norm(F_value, ord=np.inf)
 		itcount+=1
-		print('Iteration ', itcount, '\t I=',I, '\t F_norm=%0.2E' % F_norm)
+		print('Iteration ', itcount, '\t I=',Ivec, '\t F_norm=%0.2E' % F_norm)
 
 	if abs(F_norm) > tol:
 		itcount = -1 #Does not converge within 100 iterations
-	return I, itcount
+	return Ivec, itcount
 
-Multvariate_Newton(I)
+Multvariate_Newton(Ivec, Rvec)
